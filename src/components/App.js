5adcfebacf043d793726e0f5a0b36f8c16db72cf
mehdi shirazi <msh199271@gmail.com>
2020-06-03 12:54:35 +0430

    Added a ClassName function

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,8 @@
 import React from 'react'
 import Joke from "./Joke"
 import "./App.css"
-import "./Joke.css"
 
-function ClassNames() {
-    if (document.getElementById("1").id == 1){
-        let even = document.getElementById("1");
-        even.className = "jokeRed"
-    }
-}
-
-function App(){
+function App() {
     return (
         <div className="App">
             <Joke id="1"/>
--- src/components/Joke.js
+++ src/components/Joke.js
@@ -1,8 +1,22 @@
 import React from 'react'
+import "./Joke.css"
 
-function Joke () {
+function Joke (props) {
+    debugger
+    function ClassName() {
+        let ids =[]
+        ids.push(props.id)
+        for (let i=0; i < ids.length; i++){
+            if (ids[i]%2 == 0){
+                props.ClassName = "jokeRed"
+            }
+            else{
+                props.ClassName = "jokeBlue"
+            }
+        }
+    }
     return (
-        <div>
+        <div className={ClassName()}>
             <p>Questions: What do you call a boomerang that won’t come back?</p>
             <p>Answer: A stick.</p>
         </div>
